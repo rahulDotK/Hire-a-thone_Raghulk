fec1650e4896b666983d4ea7b05f0eb377a249e3
raghul <rahuldotkmay01@gmail.com>
2022-09-24 21:19:54 +0530

    Added customizable title component

--- StartUp/src/app/app.module.ts
+++ StartUp/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SectionTitleComponent } from './section-title/section-title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SectionTitleComponent
   ],
   imports: [
     BrowserModule
